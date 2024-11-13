06a5acef7443943e283c7e0072c3aea5a66d8b5a
Hogalallaop <162577882+Hogalallaop@users.noreply.github.com>
2024-11-13 13:17:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -39,9 +39,9 @@ module.exports = {
   nodes: [
     {
         name: "Server",
-        host: "ll.sleepyinsomniac.eu.org",
-        port: 80,
-        password: "youshallnotpass",
+        host: "lavahatry4.techbyte.host",
+        port: 3000,
+        password: "NAIGLAVA-dash.techbyte.host",
         reconnectTimeout: 5000,
         reconnectTries: Infinity,
         secure: false
